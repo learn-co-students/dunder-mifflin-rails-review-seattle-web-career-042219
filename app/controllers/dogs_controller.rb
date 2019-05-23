7d76abfae3cb216ce9d89622810ac947e167b526
github username <github email address>
2019-05-23 10:44:27 -0700

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,20 @@
 class DogsController < ApplicationController
+
+    before_action :set_dog, only: [:show, :edit, :update]
+
+    def index
+        @dogs = Dog.all 
+    end
+
+
+
+    private
+
+    def dogs_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
+
+    def set_dog
+        @dog = Dog.find(params[:id])
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,44 @@
 class EmployeesController < ApplicationController
+    before_action :set_employee, only:[:show, :edit, :update]
+
+    def index
+        @employees = Employee.all
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+
+        if @employee.valid?
+            @employee.save
+            redirect_to @employee
+        else
+            render :new
+        end
+    end
+
+    def update
+
+
+        if @employee.valid?
+            @employee.update!
+            redirect_to @employee
+        else
+            render :new
+        end
+    end
+
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :title, :alias, :office, :img_url, :dog_id)
+    end
+
+    def set_employee
+        @employee = Employee.find(params[:id])
+    end
 end
